37ab331486cb52adc261b9330257413e2f311c58
tea-eclair <148536484+tea-eclair@users.noreply.github.com>
2024-03-12 15:35:57 +0500

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,93 +1,81 @@
-using System;
+using System.Data;
+using System.DirectoryServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace Safe
+namespace safee
 {
-    public partial class MainWindow : Window
-    {
-        private bool isDragging = false;
-        private Point lastPosition;
-        private double dialRotationAngle = 0;
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+public partial class MainWindow : Window;
+
+
+    private int password = 99;
+    private int result = 0;
+
+    private Point startPoint;
+    private bool isDragging = false;
 
         public MainWindow()
         {
             InitializeComponent();
-            InitializeDialRotation();
+
+            krug.MouseLeftbuttonDown += Krug_MouseLeftButtonDown;
+            krug.MouseMove =+ Krug_MouseMove;
+            krug.MouseLeftbuttonUp += Krug_MouseLeftButtonUp;
         }
 
-        private void InitializeDialRotation()
-        {
-            krug.MouseLeftButtonDown += Krug_MouseLeftButtonDown;
-            krug.MouseMove += Krug_MouseMove;
-            krug.MouseLeftButtonUp += Krug_MouseLeftButtonUp;
-        }
-
-        private void Krug_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            isDragging = true;
-            lastPosition = e.GetPosition(this);
-        }
-
-        private void Krug_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (isDragging)
-            {
-                Point currentPosition = e.GetPosition(this);
-
-                // Вычисляем разницу в координатах между текущей позицией и предыдущей позицией мыши
-                double deltaX = currentPosition.X - lastPosition.X;
-                double deltaY = currentPosition.Y - lastPosition.Y;
-
-                // Вычисляем угол вращения в радианах с помощью арктангенса
-                double angleRad = Math.Atan2(deltaY, deltaX);
-
-                // Преобразуем угол в градусы
-                double angleDeg = angleRad * (180 / Math.PI);
-
-                // Применяем вращение к циферблату
-                dialRotationAngle += angleDeg;
-                RotateDial(dialRotationAngle);
-
-                // Обновляем номер на циферблате
-                UpdateNumberOnDial();
-
-                // Обновляем позицию мыши
-                lastPosition = currentPosition;
-            }
-        }
-
-        private void Krug_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            isDragging = false;
-        }
-
-        private void RotateDial(double angle)
-        {
-            ((RotateTransform)krug.RenderTransform).Angle = angle;
-        }
-
-        private void UpdateNumberOnDial()
-        {
-            // Пересчитываем номер на циферблате на основе угла вращения
-            double angle = dialRotationAngle % 360;
-            if (angle < 0)
-                angle += 360;
-
-            // Приводим угол к диапазону от 0 до 100
-            if (angle < 0)
-                angle += 360;
-            else if (angle > 360)
-                angle -= 360;
-
-            // Рассчитываем номер на циферблате в диапазоне от 0 до 99
-            int number = (int)(angle / 3.6); // 360 градусов / 100 делений = 3.6 градуса на одно деление
-
-            // Обновляем текстовый блок с номером
-            number1.Text = "Number: " + number.ToString();
-        }
+        private void Krug_MouseLeftButtonDown (object sender, MouseButtonEventArgs e)
+    {
+        startPoint = new Point (Canvas.Getleft(this) + this.ActualWidth /2, Canvas.GetTop(this) + this.ActualHeight /2);
+        isDragging = true;
     }
+
+    private void Krug_MouseMove(object sender, MouseEventArgs e)
+
+    {
+        if (isDragging)
+        {
+            Point currentPoint = e.GetPosition (MainCanvas);
+            Vector delta = currentPoint - startPoint;
+            double angle = Math.Atan2(delta.Y, delta.X) * (180 / Math.PI);
+            angle = (angle + 360) % 360;
+            double mappedNumber = Map(angle, 0, 360, 0, 99);
+            result= Int32.Parse(mappedNumber.ToString("0"));
+            ((RotateTransform)krug.RenderTransform).Angle = angle;
+    }
+}
+
+private void Button_Click(object sender, RoutedEventArgs e){
+    if (Int32.TryParse(mynumber.Text, out int numValue)){
+        password = numValue;
+        mynumber.Text = "Пароль задан. Угадайте: " + password;
+        MessageBox.Show("Введите число");
+    }
+}
+
+private void Krug_MouseLeftButtonUp (object sender, MouseButtonEventArgs e)
+{
+    isDragging = false;
+    var checker = PasswordCheck(password, resulr);
+    if (checker){
+    MessageBox.Show("Все правильно!");    
+    }
+    else{
+        MessageBox.Show("Не правильно. Вы ввели:" + result.ToString());
+    }
+}
+
+private bool PasswordCheck(int a, int b){
+    return a == b;
+}
 }
