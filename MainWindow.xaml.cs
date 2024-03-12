6874aeaffb2c2c4e71931213b454e07d26ad6c7d
Inokentiy78 <148535452+Inorentiy78@users.noreply.github.com>
2024-03-12 14:12:30 +0500

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,65 +1,93 @@
-﻿using System.Windows;
+using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 
-namespace Organy_Drag_and_Drop
+namespace Safe
 {
     public partial class MainWindow : Window
     {
+        private bool isDragging = false;
+        private Point lastPosition;
+        private double dialRotationAngle = 0;
+
         public MainWindow()
         {
             InitializeComponent();
-
-            // Добавляем обработчики событий для изображений
-            AddDragAndDropEvents(True_Card1);
-            AddDragAndDropEvents(True_Card2);
-            AddDragAndDropEvents(True_Card3);
-            AddDragAndDropEvents(True_Card4);
-            AddDragAndDropEvents(False_Card1);
-            AddDragAndDropEvents(False_Card2);
-            AddDragAndDropEvents(False_Card3);
+            InitializeDialRotation();
         }
 
-        // Метод для добавления обработчиков событий для изображения
-        private void AddDragAndDropEvents(Image image)
+        private void InitializeDialRotation()
         {
-            image.MouseLeftButtonDown += Image_MouseLeftButtonDown;
-            image.MouseMove += Image_MouseMove;
-            image.MouseLeftButtonUp += Image_MouseLeftButtonUp;
+            krug.MouseLeftButtonDown += Krug_MouseLeftButtonDown;
+            krug.MouseMove += Krug_MouseMove;
+            krug.MouseLeftButtonUp += Krug_MouseLeftButtonUp;
         }
 
-        private bool isDragging = false;
-        private Point originalMousePosition;
-        private TranslateTransform originalTranslation;
-
-        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void Krug_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             isDragging = true;
-            originalMousePosition = e.GetPosition(null);
-            originalTranslation = new TranslateTransform(Canvas.GetLeft((UIElement)sender), Canvas.GetTop((UIElement)sender));
-            ((UIElement)sender).RenderTransform = originalTranslation;
-            ((UIElement)sender).CaptureMouse();
+            lastPosition = e.GetPosition(this);
         }
 
-        private void Image_MouseMove(object sender, MouseEventArgs e)
+        private void Krug_MouseMove(object sender, MouseEventArgs e)
         {
             if (isDragging)
             {
-                Point currentPosition = e.GetPosition(null);
-                Vector delta = Point.Subtract(currentPosition, originalMousePosition);
-                originalTranslation.X += delta.X;
-                originalTranslation.Y += delta.Y;
-                Canvas.SetLeft((UIElement)sender, originalTranslation.X);
-                Canvas.SetTop((UIElement)sender, originalTranslation.Y);
-                originalMousePosition = currentPosition;
+                Point currentPosition = e.GetPosition(this);
+
+                // Вычисляем разницу в координатах между текущей позицией и предыдущей позицией мыши
+                double deltaX = currentPosition.X - lastPosition.X;
+                double deltaY = currentPosition.Y - lastPosition.Y;
+
+                // Вычисляем угол вращения в радианах с помощью арктангенса
+                double angleRad = Math.Atan2(deltaY, deltaX);
+
+                // Преобразуем угол в градусы
+                double angleDeg = angleRad * (180 / Math.PI);
+
+                // Применяем вращение к циферблату
+                dialRotationAngle += angleDeg;
+                RotateDial(dialRotationAngle);
+
+                // Обновляем номер на циферблате
+                UpdateNumberOnDial();
+
+                // Обновляем позицию мыши
+                lastPosition = currentPosition;
             }
         }
 
-        private void Image_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        private void Krug_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             isDragging = false;
-            ((UIElement)sender).ReleaseMouseCapture();
+        }
+
+        private void RotateDial(double angle)
+        {
+            ((RotateTransform)krug.RenderTransform).Angle = angle;
+        }
+
+        private void UpdateNumberOnDial()
+        {
+            // Пересчитываем номер на циферблате на основе угла вращения
+            double angle = dialRotationAngle % 360;
+            if (angle < 0)
+                angle += 360;
+
+            // Приводим угол к диапазону от 0 до 100
+            if (angle < 0)
+                angle += 360;
+            else if (angle > 360)
+                angle -= 360;
+
+            // Рассчитываем номер на циферблате в диапазоне от 0 до 99
+            int number = (int)(angle / 3.6); // 360 градусов / 100 делений = 3.6 градуса на одно деление
+
+            // Обновляем текстовый блок с номером
+            number1.Text = "Number: " + number.ToString();
         }
     }
 }
